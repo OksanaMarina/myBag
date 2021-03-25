a9b78b7d892faef07c38b04184f15a5a9f4d2f7e
Ivan <needorogo@gmail.com>
2021-03-25 15:14:08 +0200

    25

--- my.js
+++ my.js
@@ -1,14 +1,20 @@
-const nuberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",0);
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",0);
 const personalMovieDB = {
-  count:numberOfFilms,
-  movies:moviesV = {},
-  actors:actorsV = {},
-  genres:genresA = [],
-  privat:false,
+   count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 
 }
-const lastFilm = prompt("Один из последних просмотренных фильмовф?","название");
-const evaluateFilm = +prompt("На сколько оцените его?",0);
-moviesV[lastFilm];
-lastFilm[evaluateFilm];
+console.log(personalMovieDB);
+const lastFilm = prompt("Один из последних просмотренных фильмов?","название"),
+      evaluateFilm = +prompt("На сколько оцените его?",0),
+      lastFilm1 = prompt("Один из последних просмотренных фильмов?","название"),
+      evaluateFilm1 = +prompt("На сколько оцените его?",0);
+
+    personalMovieDB.movies[lastFilm] = evaluateFilm;
+    personalMovieDB.movies[lastFilm1] = evaluateFilm1;
+console.log(personalMovieDB);
 
