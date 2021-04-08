2808af4d7a37a0ae53379ce9e99b1c97650a9647
Ivan <needorogo@gmail.com>
2021-04-08 15:57:49 +0300

    0804

--- theory.js
+++ theory.js
@@ -1,1 +1,25 @@
 "use strict";
+const grq = 50;
+if (grq < 49){
+    console.log('Error');
+} else if (grq > 100){
+    console.log('Error');
+} else {
+    console.log(grq);
+}
+(grq === 50) ? console.log('Ok!') : console.log('Error');
+
+switch (grq) {
+    case 49:
+        console.log('Хи-хи!');
+    break;
+    case 100:
+        console.log('Тоже не пойдет!');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не вышло)');
+}
+
